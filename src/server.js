44fdbb59c8c0cb146fb71db9f59d4c0cb2957428
Christopher Wilson <chris@abitofcode.com>
2020-05-26 12:26:08 +0100

    Add etags and expires headers

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = false;// NODE_ENV === 'development';
+console.log("MODE: " + dev)
+
 
 express() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('static', { dev }),
+		// express.static('static'),
+		sirv('static', { dev, etag: true, maxAge: 3600 }),
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
